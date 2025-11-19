28509a32f2c119d9cceacda375c8f2d4138ceb11
MAbdullah-dev <mabdullahwebdev@gmail.com>
2025-11-20 02:37:00 +0500

    frontend of  main site page

--- src/app/(auth)/auth/page.tsx
+++ src/app/(auth)/auth/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Login from "@/components/Auth/login";
-import Register from "@/components/Auth/register";
+import Login from "@/components/Auth/Login";
+import Register from "@/components/Auth/Register";
 
 const AuthPage = () => {
   return (
