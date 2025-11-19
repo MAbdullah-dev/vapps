26e0a76d62332e80444231022d3abdf156bf8407
MAbdullah-dev <mabdullahwebdev@gmail.com>
2025-11-19 20:44:19 +0500

    change in folder structure

---
+++ src/app/(auth)/auth/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "@/components/Auth/login";
+import Register from "@/components/Auth/register";
+
+const AuthPage = () => {
+  return (
+    <div>
+      <Login />
+      <Register />
+    </div>
+  );
+};
+
+export default AuthPage;
